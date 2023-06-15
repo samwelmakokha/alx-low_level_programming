380c3403bfc532126222f373986aac1ce92cc7ce
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-15 12:04:00 -0700

    added 4-puts.c and 5-printf.c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -main function of the  program
+ *
+ * Return: a program should return zero error
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
