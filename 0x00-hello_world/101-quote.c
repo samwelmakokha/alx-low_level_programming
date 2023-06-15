cbe54042a0aa7d924d79ebc33bcd9ed5547e6770
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-15 13:28:19 -0700

    added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function of the program
+ *
+ * Return: Always written 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
