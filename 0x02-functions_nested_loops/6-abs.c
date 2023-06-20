957380dec746640c9bc4857b9e481df99570c4fa
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-20 12:24:43 -0700

    added a file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - computes the absolute value of an integer
+ * @c: the init to check
+ *
+ * Return: absolute value
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
