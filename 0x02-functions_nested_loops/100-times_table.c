c411e9e1216fa3039523af22ce61e65693e2fc53
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-20 14:21:59 -0700

    added a file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,40 +5,35 @@
  * @n: The value
  */
 void print_times_table(int n)
-	{
-	int i, j, k;
+{
+	int a, b, c;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (a = 0; a <= n; a++)
 		{
-			_putchar(48);
-			for (j = 1; j <= n; j++)
-
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
-				k = i * j;
-				_putchar(44);
-				_putchar(32);
-				if (k <= 9)
+				_putchar(',');
+				_putchar(' ');
+				c = a * b;
+				if (c <= 99)
+					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+				if (c >= 100)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(k + 48);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10)) % 10 + '0');
 				}
-				else if (k <= 99)
+				else if (c <= 99 && c >= 10)
 				{
-					_putchar(32);
-					_putchar((k / 10) + 48);
-					_putchar((k % 10) + 48);
+					_putchar((c / 10) + '0');
 				}
-				else
-				{
-					_putchar(((k / 100) % 10) + 48);
-					_putchar(((k / 10) % 10) + 48);
-					_putchar((k % 10) + 48);
-				}
-				_putchar('\n');
+				_putchar((c % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
