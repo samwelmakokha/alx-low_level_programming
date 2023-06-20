af697f20459a9f7066ac4ea0f1b2de0532566641
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-20 08:40:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - main entry point of the function
+ *
+ * Return: returns void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
