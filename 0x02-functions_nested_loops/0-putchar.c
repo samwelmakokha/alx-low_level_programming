41a6e8ba61c9850f9b7b9681f1f40161612c2407
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-20 08:29:16 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * main - main function of the program
+ *
+ * Return: returns 0 when the prograam executes successfully
+ */
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
