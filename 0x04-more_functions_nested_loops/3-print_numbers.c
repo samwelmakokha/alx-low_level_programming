e0bfe6becf53aa49b8085763859a2b784ab9f4ea
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-22 10:22:43 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - outputs numbers 0-9
+* Return: returns success (0)
+*/
+void print_numbers(void)
+{
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+		return (0);
+	}
+
