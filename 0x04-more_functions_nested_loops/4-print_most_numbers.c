f6aea213c56b94adb9ab885380e8eea67f4f42b8
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-22 10:47:19 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - outputs numbers 0-9
+* Return: returns success (0)
+*/
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+
