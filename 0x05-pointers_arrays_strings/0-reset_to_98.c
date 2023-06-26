db8b02dd0efcbc4f2054f98ab48cec6d1d3d89fb
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-26 06:45:53 -0700

    add 0-reset file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: 0 for success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
