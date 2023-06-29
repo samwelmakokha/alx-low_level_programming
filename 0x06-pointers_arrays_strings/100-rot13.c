fbc21a1d5cb9d7220578332ca010842668f53d16
samwelmakokha <samwelmakokha10@gmail.com>
2023-06-29 04:56:59 -0700

    add 100-rot13 file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encode string using  rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char b[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char c[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == b[j])
+			{
+				s[i] = c[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
