c6f8070dc90c4c7b8d34103996b581011b2e6533
samwelmakokha <samwelmakokha10@gmail.com>
2023-07-03 10:23:09 -0700

    add 3-strspn file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				n++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
