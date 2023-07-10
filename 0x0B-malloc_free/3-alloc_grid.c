f506aaddf1bbb4af7756676391425dd59860bb75
samwelmakokha <samwelmakokha10@gmail.com>
2023-07-10 12:36:55 -0700

    add 3-alloc_grid file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - nested loop to make grid.
+ * @width: width value
+ * @height: height value
+ * Return: pointer to 2 dimension array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **mem;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	mem = malloc(sizeof(int *) * height);
+
+	if (mem == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		mem[x] = malloc(sizeof(int) * width);
+
+		if (mem[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(mem[x]);
+
+			free(mem);
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mem[x][y] = 0;
+	}
+
+	return (mem);
+}
